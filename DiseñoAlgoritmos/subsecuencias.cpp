086d1d7872e9eef78e35eded920f6f789aaae49a
Joan Andoni <mcr_joan@hotmail.com>
2017-10-03 05:59:53 +0100

    Intento de subsecuencia

---
+++ DiseñoAlgoritmos/subsecuencias.cpp
@@ -0,0 +1,31 @@
+/* Complejidad = O()
+
+
+Input:    Text with n characters and Pattern with m characters
+Output: Index of the first substring of T matching P */
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+std::string boyerymoore(std::string T, int n, std::string p, int m ){
+  int i = m-1;
+  int j = m-1;
+  while( i < n-1 ){
+    if(p[j] == T[i]){
+      if(j==0){
+        return i;
+      }else{
+        i--;
+        j--;
+      }
+    }else{
+      i = i + m - min(j, 1 + last[T[i]]);
+      j = m - 1;
+    }
+  }
+  return "no match";
+}
